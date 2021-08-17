11e733ea3e92dd09e62e7a4ca908ab48bdd0cb21
bharath <bharathkumargoud73@gmail.com>
2021-08-17 21:32:34 +0530

    fixed problem

--- src/components/App.js
+++ src/components/App.js
@@ -2,9 +2,14 @@ import React, {Component, useState} from "react";
 import "./../styles/App.css";
 
 function App() {
+  const [show,setShow]=useState(false);
   return (
     <div id="main">
-      // Do not alter the main div
+       {/* Do not alter the main div */}
+      <button id="click" onClick={()=>{
+        setShow({show:true});
+      }}>Click me</button>
+      {show?(<p id="para">Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy</p>):null}
     </div>
   );
 }
